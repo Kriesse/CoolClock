f0f9796b0272dad4c68e59f1f8ff929ad4ad27ae
Ger Hobbelt <ger@hobbelt.com>
2011-01-19 05:35:30 +0100

    another MSIE+excanvas fix: counterclockwise plotted arcs have an issue (see excanvas code & comments), but it does NOT fail when the arc is plotted clockwise, so we do that now. excanvas b0rks when the angles are not swapped as well, hence the exchange of the 0 and 2*PI arguments.

--- coolclock.js
+++ coolclock.js
@@ -140,9 +140,9 @@ CoolClock.prototype = {
 			this.ctx.lineWidth = this.ctx.lineWidth * this.scale;
 		}
 
-		this.ctx.arc(x, y, skin.radius, 0, 2*Math.PI, false);
+		this.ctx.arc(x, y, skin.radius, 2*Math.PI, 0, true);
 
-		if (CoolClock.config.isIE) {
+		if (CoolClock.config.isIE && false) {
 			// excanvas doesn't close the circle so let's fill in the tiny gap
 			this.ctx.arc(x, y, skin.radius, -0.1, 0.1, false);
 		}
